bf8a360019a136e64e6f6f57852fade1256d4ddd
YellowMay <yellow.may.21@gmail.com>
2025-02-20 13:04:19 +0100

    updated

--- components/IntroJumbotron.tsx
+++ components/IntroJumbotron.tsx
@@ -1,36 +1,47 @@
-import Link from 'next/link';
-import { FC } from 'react';
+import Link from "next/link";
+import { FC } from "react";
 
-const badges = ['NextJs', 'ReactJs', 'NodeJs'];
+const badges = [
+  "NextJs",
+  "ReactJs",
+  "Remix",
+  "Astro",
+  "VueJs",
+  "React Native",
+  "NodeJs",
+  "Laravel",
+  "MongoDB",
+  "MySQL",
+];
 
 const IntroJumbotron: FC = () => {
-	return (
-		<section className='intro'>
-			<div>
-				<div className='my-info'>
-					<h1>Onyekwere Precious</h1>
-					<p>Javascript Web Developer</p>
+  return (
+    <section className="intro">
+      <div>
+        <div className="my-info">
+          <h1>Onyekwere Precious</h1>
+          <p>Full Stack Web/Mobile Developer</p>
 
-					<div className='badge-list'>
-						{badges.map(badge => (
-							<span key={badge} className='badge'>
-								{badge}
-							</span>
-						))}
-					</div>
-				</div>
+          <div className="badge-list">
+            {badges.map((badge) => (
+              <span key={badge} className="badge">
+                {badge}
+              </span>
+            ))}
+          </div>
+        </div>
 
-				<div className='links'>
-					<Link href='/projects'>
-						<a className='left-link'>My Projects</a>
-					</Link>
-					<Link href='/contact'>
-						<a className='right-link'>Hire Me</a>
-					</Link>
-				</div>
-			</div>
-		</section>
-	);
+        <div className="links">
+          <Link href="/projects">
+            <a className="left-link">My Projects</a>
+          </Link>
+          <Link href="/contact">
+            <a className="right-link">Hire Me</a>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default IntroJumbotron;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,36 @@
-import { Fragment } from 'react';
-import type { NextPage } from 'next';
-import IntroJumbotron from 'components/IntroJumbotron';
-import ProjectCard from 'components/ProjectCard';
-import Link from 'next/link';
-import projects from 'data/projects.json';
+import { Fragment } from "react";
+import type { NextPage } from "next";
+import IntroJumbotron from "components/IntroJumbotron";
+import ProjectCard from "components/ProjectCard";
+import Link from "next/link";
+import projects from "data/projects.json";
 
 const Home: NextPage = () => {
-	return (
-		<Fragment>
-			<main className='h-full'>
-				<IntroJumbotron />
+  return (
+    <Fragment>
+      <main className="h-full">
+        <IntroJumbotron />
 
-				<section className='star_projects'>
-					<h2>My Star Project(s)</h2>
+        <section className="star_projects">
+          <h2>Personal Project(s)</h2>
 
-					<ul className='card-wrapper'>
-						{projects
-							.filter(({ star }) => star)
-							.map(project => (
-								<ProjectCard key={project.id} {...project} />
-							))}
-					</ul>
+          <ul className="card-wrapper">
+            {projects
+              .filter(({ star }) => star)
+              .map((project) => (
+                <ProjectCard key={project.id} {...project} />
+              ))}
+          </ul>
 
-					<div className='link'>
-						<Link href='/projects'>
-							<a>view all projects</a>
-						</Link>
-					</div>
-				</section>
-			</main>
-		</Fragment>
-	);
+          <div className="link">
+            <Link href="/projects">
+              <a>view all projects</a>
+            </Link>
+          </div>
+        </section>
+      </main>
+    </Fragment>
+  );
 };
 
 export default Home;
